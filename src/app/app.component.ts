a723f9bf27ab47f9792285052438a2f410acffe7
LanaNew <l4n.steen@gmail.com>
2025-07-21 21:58:32 +0400

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
-  title = 'ng1';
+
+
+  title = 'ng1';  /// string
+  x = 10  ///// number
+  y : string = ""
+
+
+
+  fun(num1:number){
+      console.log(num1)
+  }
+
+  
+
 }
+
+
+
+
+
+
+
+
+
+
