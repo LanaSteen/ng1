15b8f71362033ae5d570a83b7f46101a3cf2e73b
LanaNew <l4n.steen@gmail.com>
2025-07-23 22:01:23 +0400

    ng 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,16 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { count } from "rxjs";
+import { CommonModule } from "@angular/common";
+
 
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -15,19 +19,67 @@ import { FooterComponent } from "./footer/footer.component";
 
 export class AppComponent {
 
+  titleColor = {
+    'color' : "blue",
+    'font-style' : 'italic'
+  }
+  hint = "შეიყვანე პაროლი"
+  title = 'ჩემი პირველი ანგულარ აპლიკაცია';  /// string
 
-  title = 'ng1';  /// string
+  clickCount = 1
+  inputType ="password"
+
   x = 10  ///// number
   y : string = ""
-
-
+  
+  imgUrl2 ="https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dW5zcGxhc2glMjBhcHB8ZW58MHx8MHx8fDA%3D"
+  imgUrl1 = "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?w=620&auto=format&fit=crop&q=60&ixlib=rb-4.1.0"
+  imgUrl = "https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dW5zcGxhc2glMjBhcHB8ZW58MHx8MHx8fDA%3D"
+  
+  isActive = false
 
   fun(num1:number){
       console.log(num1)
   }
 
-  
 
+  // show(){
+  //   this.clickCount++
+  //   if(this.clickCount%2==0){
+  //     this.imgUrl = this.imgUrl1
+  //   }
+  //   else {
+  //      this.imgUrl = this.imgUrl2
+  //   }
+  // }
+
+
+   show(){
+    this.imgWidth == 300 ? this.imgWidth = 500 : this.imgWidth = 300
+    this.toggle()
+    this.imgUrl == "https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dW5zcGxhc2glMjBhcHB8ZW58MHx8MHx8fDA%3D" 
+    ? this.imgUrl ="https://images.unsplash.com/photo-1449034446853-66c86144b0ad?w=620&auto=format&fit=crop&q=60&ixlib=rb-4.1.0"
+    : this.imgUrl =  "https://plus.unsplash.com/premium_photo-1681422570054-9ae5b8b03e46?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8dW5zcGxhc2glMjBhcHB8ZW58MHx8MHx8fDA%3D"
+  }
+
+
+  imgWidth = 300
+
+  z = 1
+
+  toggle(){
+    this.z == 1 ? this.z=0 : this.z = 1
+    console.log(this.z)
+  }
+  
+  
+ change(){
+  this.titleColor['color']="green"
+   this.titleColor['font-style']=""
+  this.title ="ჩვენი მეორე აპლიკაცია"
+  this.hint = "შეიყვანე იმეილი"
+  this.inputType = "email"
+ }
 }
 
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+ showMobileMenu = false
 
+ showHide(){
+   this.showMobileMenu == false ?  this.showMobileMenu = true :  this.showMobileMenu = false
+ }
 }
