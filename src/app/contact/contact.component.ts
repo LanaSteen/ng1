45caf2ca1c016efee1ef38c61a3ec2288f5db57c
LanaNew <l4n.steen@gmail.com>
2025-08-11 22:34:39 +0400

    ang 5

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,46 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Gender, User } from '../Models/user';
+
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+
+  // title : string | number | boolean | null | object | undefined = 200
+  // title : any
+
+  userName = ""
+  userNameShow =""
+
+  userObj = new User
+  userArr : User[] =[]
+
+
+  save(){
+    console.log(this.userName);
+    this.userNameShow = this.userName
+  }
+
+
+  liveSearch(){
+    console.log(this.userName)
+  }
+
+
+  saveObj(){
+    this.userObj.gender = Gender.male
+    this.userArr.push(this.userObj)
+    this.userObj = new User
+    console.log(this.userArr)
+  }
 }
+
+
+
