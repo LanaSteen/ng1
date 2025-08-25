b7b6d743b2a5218b18fa2d0628b1ad94b0363c49
LanaNew <l4n.steen@gmail.com>
2025-08-25 22:31:58 +0400

    fixed error dialog

--- src/app/Services/api.service.ts
+++ src/app/Services/api.service.ts
@@ -4,45 +4,43 @@ import { catchError, throwError } from 'rxjs';
 import { ErrorService } from './error.service';
 
 @Injectable({
-  providedIn: 'root'
-})  //////// singletone
+  providedIn: 'root',
+}) //////// singletone
 export class ApiService {
+  constructor(private http: HttpClient, private err: ErrorService) {}
 
-  constructor(private http : HttpClient, private err : ErrorService) { }
-
-
-  baseUrl = "https://restaurant.stepprojects.ge/api/"   ///Baskets/GetAll    /// Products/GetAll
+  baseUrl = 'https://restaurant.stepprojects.ge/api/'; ///Baskets/GetAll    /// Products/GetAll
 
   // getData(url : string){
   //   return this.http.get(this.baseUrl + url).pipe(
   //      catchError(this.errorHandling)
   //   )
   // }
-    getData(url : string){
+  //   getData(url : string){
+  //   return this.http.get(url).pipe(
+
+  //      catchError(this.errorHandling)
+  //   )
+  // }
+  getData(url: string) {
     return this.http.get(url).pipe(
-       catchError(this.errorHandling)
-    )
-  }
-
-
-  postData(url : string, obj : any){
-    return this.http.post(url, obj).pipe(
-       catchError(this.errorHandling)
-    )
-  }
-
-
-  private errorHandling (err : HttpErrorResponse) {
-      return throwError(() => {
-         this.err.showErr()
-        Error("somethong went werog, try later")
-        // alert("somethong went werog, try later")
-       
+      catchError((err: HttpErrorResponse) => {
+        this.err.showErr();
+        return throwError(() => new Error('Something went wrong, try later'));
       })
+    );
   }
 
+  postData(url: string, obj: any) {
+    return this.http.post(url, obj).pipe(catchError(this.errorHandling));
+  }
+
+  private errorHandling(err: HttpErrorResponse) {
+    return throwError(() => {
+      Error('somethong went werog, try later');
+      // alert("somethong went werog, try later")
+    });
+  }
 }
-
-
 
 // htttpClinet
